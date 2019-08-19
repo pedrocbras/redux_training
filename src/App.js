1e189fc0af1833e636a193e133c5d5c3dbf70ff5
pedro_bras <pedro.c.bras@gmail.com>
2019-08-19 12:31:01 +0200

    changes hello world greeting to state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Container, Header } from "semantic-ui-react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    greeting: 'Hello World from Component State'
+  }
   render() {
     return (
       <>
         <Container>
-          <Header as="h1">Hello World</Header>
+          <Header as="h1">{this.state.greeting}</Header>
         </Container>
       </>
     );
