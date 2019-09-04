5d98a5bdc4b69d332a2ee8ad9a35d5e070e9904d
pedro_bras <pedro.c.bras@gmail.com>
2019-09-04 20:01:44 +0200

    adds proposed greeting action to reducer

--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,11 +1,16 @@
 import initialState from "../store/initialState";
 
 const rootReducer = (state = initialState, action) => {
-  if (action.type === "CHANGE_GREETING") {
+  if (action.type === 'PROPOSED_GREETING'){
     return {
       ...state,
-      greeting: action.payload
+      proposed_greeting: action.payload
     }
+  } else if (action.type === "CHANGE_GREETING") {
+    return {
+      ...state,
+      greeting: state.proposed_greeting
+    };
   } else {
     return state;
   }
